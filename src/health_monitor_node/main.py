416b8ede5197e6dc07cbf0705e955888f07219ac
Tom <TSLourenco@WPI.edu>
2023-01-20 00:27:57 +0000

    Fixed fault sorting.

--- src/health_monitor_node/main.py
+++ src/health_monitor_node/main.py
@@ -1,38 +1,41 @@
 #!/usr/bin/env python3
 
-import tf2_ros
 import rospy
 from threading import Thread
 
-from frc_robot_utilities_py_node.frc_robot_utilities_py import *
-from frc_robot_utilities_py_node.RobotStatusHelperPy import RobotStatusHelperPy, Alliance, RobotMode, BufferedROSMsgHandlerPy
 from ck_ros_msgs_node.msg import Fault, Health_Monitor_Control, Health_Monitor_Status
+from frc_robot_utilities_py_node.frc_robot_utilities_py import *
+from frc_robot_utilities_py_node.RobotStatusHelperPy import BufferedROSMsgHandlerPy
+
 
 def ros_func():
 
-    control_sub = BufferedROSMsgHandlerPy(Health_Monitor_Control)
-    control_sub.register_for_updates("HealthMonitorControl")
-    status_pub = rospy.Publisher(name="HealthMonitorStatus", data_class=Health_Monitor_Status, queue_size=50, tcp_nodelay=True)
+    control_subscriber = BufferedROSMsgHandlerPy(Health_Monitor_Control)
+    control_subscriber.register_for_updates("HealthMonitorControl")
+
+    status_publisher = rospy.Publisher(name="HealthMonitorStatus", data_class=Health_Monitor_Status, queue_size=50, tcp_nodelay=True)
 
     rate = rospy.Rate(20)
 
-    rospy.loginfo("Health monitoring")
-    faultlist = []
+    fault_list = []
+
     while not rospy.is_shutdown():
-        if control_sub.get() is not None:
-            if control_sub.get().acknowledge:
-                faultlist.clear()
-            for fault in control_sub.get().faults:
-                if fault not in faultlist:
-                    faultlist.append(fault)
 
-        pubmsg = Health_Monitor_Status()
-        pubmsg.faults = faultlist.sort(reverse = True, key = "priority")
-        status_pub.publish(pubmsg)
 
+        if control_subscriber.get() is not None:
+
+            if control_subscriber.get().acknowledge:
+                fault_list.clear()
+
+            for fault in control_subscriber.get().faults:
+                if fault not in fault_list:
+                    fault_list.append(fault)
+
+        status = Health_Monitor_Status()
+        status.faults = sorted(fault_list, key=lambda fault: fault.priority, reverse=True)
+        status_publisher.publish(status)
 
         rate.sleep()
-
 
 
 def ros_main(node_name):
